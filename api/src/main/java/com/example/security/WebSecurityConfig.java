45d80616c9fe9e670efc3a464bff6346d4d1b31b
Piotr Damrych <piotr.damrych2002@gmail.com>
2023-05-14 19:50:26 +0200

    added keycloak and basic security

---
+++ api/src/main/java/com/example/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    public static final String ADMIN = "admin";
+    public static final String USER = "user";
+    
+    private final JwtAuthConverter jwtAuthConverter;
+
+    public WebSecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/movie", "movie/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                .anyRequest().authenticated();
+        http.oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().and().csrf().disable();
+        return http.build();
+    }
+
+}
