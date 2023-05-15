4918f2143c3eab822a9645cbf3cacdd349a573c5
Piotr Damrych <p.damrych.683@studms.ug.edu.plgit config --global user.name Piotr>
2023-05-15 22:34:40 +0200

    some work, not done, but keycloak works with usercontroller

---
+++ api/src/main/java/com/example/user/KeycloakConfig.java
@@ -0,0 +1,22 @@
+package com.example.user;
+
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+    @Bean
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl("http://localhost:8080")
+                .realm("master")
+                .clientId("admin-cli")
+                .username("admin")
+                .password("admin")
+                .grantType(OAuth2Constants.PASSWORD)
+                .build();
+    }
+}
