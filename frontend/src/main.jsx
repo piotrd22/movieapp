f36e732bad15d55ebc3b5e3df7a28d63bafb55fb
Piotr Damrych <piotr.damrych2002@gmail.com>
2023-05-22 19:00:14 +0200

    added pkce

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,10 @@ import keycloak from "./Keycloak.js";
 import App from "./App.jsx";
 import "./index.css";
 
+const initOptions = { pkceMethod: 'S256' };
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <ReactKeycloakProvider authClient={keycloak}>
+  <ReactKeycloakProvider authClient={keycloak} initOptions={initOptions}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
